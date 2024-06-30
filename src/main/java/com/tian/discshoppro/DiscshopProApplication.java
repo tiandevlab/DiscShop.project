7476a95e7a9377118393d6415ee6333031a8ac44
Tianyi.Huang <fh.frankhuang@gmail.com>
2024-06-30 11:08:06 +0100

    Initial commit: Spring Boot project setup

---
+++ src/main/java/com/tian/discshoppro/DiscshopProApplication.java
@@ -0,0 +1,13 @@
+package com.tian.discshoppro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscshopProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscshopProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tian/discshoppro/DiscshopProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tian.discshoppro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscshopProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
