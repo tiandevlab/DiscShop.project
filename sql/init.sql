5e5849c7b5605f7f49518276f81932a4aaf73157
Tianyi.Huang <fh.frankhuang@gmail.com>
2024-07-01 22:18:00 +0100

    add a price column in sql

--- sql/init.sql
+++ sql/init.sql
@@ -8,18 +8,19 @@ CREATE TABLE IF NOT EXISTS Albums(
     artist VARCHAR(255) NOT NULL,
     release_year INT,
     cover_image_name VARCHAR(255),
-    copyright VARCHAR(255)
+    copyright VARCHAR(255),
+    price DECIMAL(10, 2) NOT NULL
 );
 
 
-INSERT INTO Albums (title, artist, release_year, cover_image_name, copyright) VALUES
-       ('Wall of Eyes', 'The Smile', 2024, 'wallofeyes.jpg', 'XL Recordings Ltd'),
-       ('The Beggar', 'Swans', 2023, 'thebeggar.jpg', 'Young God Records'),
-       ('Talk Memory', 'BADBADNOTGOOD', 2021, 'talkmemory.jpg', 'XL Recordings Ltd'),
-       ('folklore', 'Taylor Swift', 2020, 'folklore.jpg', 'Taylor Swift'),
-       ('OK Computer', 'Radiohead', 1997, 'okcomputer.jpg', 'XL Recordings Ltd'),
-       ('Bitches Brew', 'Miles Davis', 1970, 'bitchesbrew.jpg', 'Sony Music Entertainment Inc'),
-       ('Different Class', 'Pulp', 1995, 'differentclass.jpg', 'Island Records'),
-       ('Daydream Nation', 'Sonic Youth', 1988, 'daydreamnation.jpg', 'Squeaky Squawk');
+INSERT INTO Albums (title, artist, release_year, cover_image_name, copyright, price)
+VALUES ('Wall of Eyes', 'The Smile', 2024, 'wallofeyes.jpg', 'XL Recordings Ltd', 25.00),
+       ('The Beggar', 'Swans', 2023, 'thebeggar.jpg', 'Young God Records', 20.00),
+       ('Talk Memory', 'BADBADNOTGOOD', 2021, 'talkmemory.jpg', 'XL Recordings Ltd', 25.00),
+       ('folklore', 'Taylor Swift', 2020, 'folklore.jpg', 'Taylor Swift', 20.00),
+       ('OK Computer', 'Radiohead', 1997, 'okcomputer.jpg', 'XL Recordings Ltd', 25.00),
+       ('Bitches Brew', 'Miles Davis', 1970, 'bitchesbrew.jpg', 'Sony Music Entertainment Inc', 25.00),
+       ('Different Class', 'Pulp', 1995, 'differentclass.jpg', 'Island Records', 20.00),
+       ('Daydream Nation', 'Sonic Youth', 1988, 'daydreamnation.jpg', 'Squeaky Squawk', 20.00);
 
 SELECT * FROM Albums;
