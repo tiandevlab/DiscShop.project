d73ea4bbdd562410e0553aa81b18b962439892b4
tiandevlab <arsdn@duck.com>
2024-07-05 22:24:24 +0100

    Implement basic CRUD operations in AlbumController

--- src/main/java/com/tian/discshoppro/controller/AlbumController.java
+++ src/main/java/com/tian/discshoppro/controller/AlbumController.java
@@ -3,7 +3,8 @@ package com.tian.discshoppro.controller;
 import com.tian.discshoppro.model.Album;
 import com.tian.discshoppro.repository.AlbumRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,5 +18,42 @@ public class AlbumController {
         return albumRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Album> getAlbumById(@PathVariable Long id) {
+        return albumRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Album createAlbum(@RequestBody Album newAlbum) {
+        return albumRepository.save(newAlbum);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Album> updateAlbum(@PathVariable Long id, @RequestBody Album albumDetails) {
+        return albumRepository.findById(id)
+                .map(album -> {
+                    album.setTitle(albumDetails.getTitle());
+                    album.setArtist(albumDetails.getArtist());
+                    album.setReleaseYear(albumDetails.getReleaseYear());
+                    album.setCoverImageName(albumDetails.getCoverImageName());
+                    album.setPrice(albumDetails.getPrice());
+                    album.setCopyright(albumDetails.getCopyright());
+                    Album updatedAlbum = albumRepository.save(album);
+                    return ResponseEntity.ok(updatedAlbum);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteAlbum(@PathVariable Long id) {
+        return albumRepository.findById(id)
+                .map(album -> {
+                    albumRepository.delete(album);
+                    return ResponseEntity.ok().build();
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
 }
