525cf713ec3cea8180722bcd0cca7edfcdd51f70
tiandevlab <arsdn@duck.com>
2024-07-08 22:13:30 +0100

    Fixed annotation issue by adding @RestController to AlbumController

--- src/main/java/com/tian/discshoppro/controller/AlbumController.java
+++ src/main/java/com/tian/discshoppro/controller/AlbumController.java
@@ -7,7 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@RestController
+@RequestMapping("/discshoppro/albums")
 public class AlbumController {
 
     @Autowired
