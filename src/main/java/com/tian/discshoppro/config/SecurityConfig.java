a48b94ee9bb6a0b73b1cadc25ad3900b246b2c29
tiandevlab <arsdn@duck.com>
2024-07-20 21:24:28 +0100

    add a SecurityConfig class to bypass auth in  test phase

---
+++ src/main/java/com/tian/discshoppro/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.tian.discshoppro.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(requests -> requests
+                        .anyRequest().permitAll()
+                )
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
