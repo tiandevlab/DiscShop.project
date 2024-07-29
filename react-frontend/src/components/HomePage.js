896af0bee4cca9585916bb9a251c24a6e0a3fcbd
tiandevlab <arsdn@duck.com>
2024-07-29 23:17:49 +0100

    Add HomePage component for main welcome section

---
+++ react-frontend/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div className="homepage">
+            <h1>Welcome to Frank Disc</h1>
+            <p>Discover the finest music here.</p>
+        </div>
+    );
+}
+
+export default HomePage;
