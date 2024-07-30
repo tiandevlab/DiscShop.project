fd28e791a767fc51f83aefc419df4808953c2fbf
tiandevlab <arsdn@duck.com>
2024-07-30 23:23:17 +0100

    Create Header component with navigation

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import HomePage from './components/HomePage';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-      <div className="App">
-        <HomePage />
-      </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <HomePage/>
+        </div>
+    );
 }
 
 export default App;
