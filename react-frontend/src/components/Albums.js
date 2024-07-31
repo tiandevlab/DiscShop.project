b6c7d4d73adc6e58cc197937009798b246a950f9
tiandevlab <arsdn@duck.com>
2024-07-31 22:46:24 +0100

    start Albums.js

---
+++ react-frontend/src/components/Albums.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+
+function Albums() {
+    const [albums, setAlbums] = useState([]);
+}
